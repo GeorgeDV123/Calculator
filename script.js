7959dc78a97d65fde2a1f0e1943e87a06cb4ebcb
GeorgeDV <georgedevries909@hotmail.com>
2022-10-31 13:30:31 +1100

    Created html, css and js files
    basic functions to js
    
    boilerplate added to html

---
+++ script.js
@@ -0,0 +1,21 @@
+const add = function(num1, num2) {
+    result = num1 + num2;
+    return(result);
+};
+
+const subtract = function(num1, num2) {
+    result = num1 - num2;
+    return(result);
+};
+
+const divide = function(num1, num2) {
+    result = num1 / num2;
+    return(result);
+};
+
+const mulitply = function(num1, num2) {
+    result = num1 * num2;
+    return(result);
+};
+
+divide(20, 2)
